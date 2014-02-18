0bb60cfbdaa88833fda205595880e008a4de3cbd
Maciej Małecki <me@mmalecki.com>
2014-02-18 01:15:55 +0100

    Add more tests for `fiatBalance`

--- test/fiatBalanceTest.js
+++ test/fiatBalanceTest.js
@@ -25,4 +25,24 @@ describe('fiatBalance test', function() {
     }, 1 * SATOSHI_FACTOR, 100);
     assert.equal(balance, 100 / cfg.settings.lowBalanceMargin);
   });
+
+  it('should calculate balance correctly with both exchanges (trade > transfer)', function() {
+    // We have 2 bitcoins for transfer, 2000 fiat for trade, want to trade 1
+    // bitcoin for 100 fiat
+    var balance = api.fiatBalance(RATE, {
+      transferBalance: 2 * SATOSHI_FACTOR,
+      tradeBalance: 2000
+    }, 1 * SATOSHI_FACTOR, 100);
+    assert.equal(balance, 100 / cfg.settings.lowBalanceMargin);
+  });
+
+  it('should calculate balance correctly with both exchanges (transfer > trade)', function() {
+    // We have 2 bitcoins for transfer, 150 fiat for trade, want to trade 1
+    // bitcoin for 100 fiat
+    var balance = api.fiatBalance(RATE, {
+      transferBalance: 2 * SATOSHI_FACTOR,
+      tradeBalance: 150
+    }, 1 * SATOSHI_FACTOR, 100);
+    assert.equal(balance, 50 / cfg.settings.lowBalanceMargin);
+  });
 });
